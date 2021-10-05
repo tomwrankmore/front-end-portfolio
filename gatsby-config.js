a78ba74db2705532b21db6b0f6721f9cf624ebcf
tomwrankmore <tomwrankmore@gmail.com>
2021-10-05 12:22:09 +0100

    cloud features intialised in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
